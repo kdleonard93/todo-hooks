e4e635e8f364183f0faa2dee802b770360709ab7
Kyle Leonard <kdleonard@icloud.com>
2019-11-03 12:11:48 -0600

    Completed dynamic Todo with hooks

--- src/App.js
+++ src/App.js
@@ -6,18 +6,62 @@ import "./App.scss";
 function App() {
   const [todos, setTodos] = useState([
     {
-      content: "Item 1",
+      content: "I",
       isComplete: true
     },
     {
-      content: "Item 2",
+      content: "Am",
       isComplete: false
     },
     {
-      content: "Item 3",
+      content: "Legend",
       isComplete: false
     }
   ]);
+
+  function handleKeyDown(e, i) {
+    if (e.key === "Enter") {
+      createTodoAtIndex(e, i);
+    }
+    if (e.key === "Backspace" && todos[i].content === "") {
+      e.preventDefault();
+      return removeTodoAtIndex(i);
+    }
+  }
+
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: "",
+      isCompleted: false
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
+  function updateTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos =>
+      todos.slice(0, i).concat(todos.slice(i + 1, todos.length))
+    );
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
+  function toggleTodoCompleteAtIndex(index) {
+    const temporaryTodos = [...todos];
+    temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted;
+    setTodos(temporaryTodos);
+  }
 
   return (
     <div className="app">
@@ -26,10 +70,22 @@ function App() {
       </div>
       <form className="todo-list">
         <ul>
-          <div className="todo">
-            <div className="checkbox" />
-            <input type="text" value="Todo one" />
-          </div>
+          {todos.map((todo, i) => (
+            <div className={`todo ${todo.isCompleted && "todo-is-completed"}`}>
+              <div
+                className={`checkbox`}
+                onClick={() => toggleTodoCompleteAtIndex(i)}
+              >
+                {todo.isCompleted && <span>&#x2714;</span>}
+              </div>
+              <input
+                type="text"
+                value={todo.content}
+                onKeyDown={e => handleKeyDown(e, i)}
+                onChange={e => updateTodoAtIndex(e, i)}
+              />
+            </div>
+          ))}
         </ul>
       </form>
     </div>
